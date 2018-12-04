005c11c21ad4ce73945483ebb03a44a1278960a5
Avinash kumar <avinash2050kumar@gmail.com>
2018-12-04 14:45:17 +0530

    [ ADD ] Child.js file added to test parent component check child component
    Parent component also check child component while testing

---
+++ src/Child.js
@@ -0,0 +1,39 @@
+/**
+ * Sample React Native App child
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, View } from "react-native";
+
+export default class Child extends Component<Props> {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}>{'This is child data'}</Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#F5FCFF"
+    },
+    welcome: {
+        fontSize: 25,
+        textAlign: "center",
+        margin: 10
+    },
+    instructions: {
+        textAlign: "center",
+        color: "#333333",
+        marginBottom: 5
+    }
+});
